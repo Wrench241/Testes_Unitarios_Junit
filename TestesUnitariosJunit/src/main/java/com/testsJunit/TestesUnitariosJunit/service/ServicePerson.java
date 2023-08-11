fb45b50d9c0b3443af6d3e224dc33e31002785b1
wrench241 <nickalex241@gmail.com>
2023-08-11 01:24:48 -0300

    final version testes Junit

--- TestesUnitariosJunit/src/main/java/com/testsJunit/TestesUnitariosJunit/service/ServicePerson.java
+++ TestesUnitariosJunit/src/main/java/com/testsJunit/TestesUnitariosJunit/service/ServicePerson.java
@@ -3,12 +3,27 @@ package com.testsJunit.TestesUnitariosJunit.service;
 import com.testsJunit.TestesUnitariosJunit.domain.Age;
 import lombok.extern.log4j.Log4j2;
 
+import java.util.ArrayList;
+import java.util.IllformedLocaleException;
+import java.util.List;
 import java.util.Objects;
+import java.util.stream.Collectors;
 
 public class ServicePerson {
+
+    //metodo de validação da idade
     public boolean validation(Age age){
-        Objects.requireNonNull(age, "Object cant be null.");
+
+        //verifica se for nulo lança exceção
+        if(age == null){
+            throw new IllformedLocaleException("Age cant be null");
+        }
         return age.getIdade() >= 18;
+
+    }
+    //remove todos que não são adultos
+    public List<Age> removeNotAdult(List<Age> age){
+       return age.stream().filter(this::validation).collect(Collectors.toList());
 
     }
 }
--- TestesUnitariosJunit/src/test/java/com/testsJunit/TestesUnitariosJunit/TestesUnitariosJunitApplicationTests.java
+++ TestesUnitariosJunit/src/test/java/com/testsJunit/TestesUnitariosJunit/TestesUnitariosJunitApplicationTests.java
@@ -3,28 +3,61 @@ package com.testsJunit.TestesUnitariosJunit;
 import com.testsJunit.TestesUnitariosJunit.domain.Age;
 import com.testsJunit.TestesUnitariosJunit.service.ServicePerson;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
 @SpringBootTest
 class TestesUnitariosJunitApplicationTests {
+
+	private Age adult;
+	private Age notAdult;
+
+	@BeforeEach
+	public void setUp(){
+		adult = new Age(18);
+		notAdult = new Age(10);
+	}
 
 	@Test
 	@DisplayName("A person should be not adult when age is lower than 18")
 	void isAdult_when_ageReturnFalse() {
-		Age age = new Age(10);
 		ServicePerson idade = new ServicePerson();
-		Assertions.assertEquals(false, idade.validation(age));
+		Assertions.assertEquals(false, idade.validation(notAdult));
 	}
 
 	@Test
 	@DisplayName("if age is lower than 18 its false")
 	void isnotadult() {
-		Age age = new Age();
-		age.setIdade(19);
 		ServicePerson servicePerson = new ServicePerson();
-		Assertions.assertTrue(servicePerson.validation(age));
+		Assertions.assertTrue(servicePerson.validation(adult));
+
+	}
+
+	@Test
+	@DisplayName("this test to be error if result is empty")
+	void resultVoid(){
+		ServicePerson servicePerson = new ServicePerson();
+		Assertions.assertThrows(NullPointerException.class,
+				() -> servicePerson.validation(null));
+	}
+
+	@Test
+	@DisplayName("Test for validation if is greate that 18")
+	void verification(){
+		ServicePerson servicePerson = new ServicePerson();
+		List<Age> list = new ArrayList<>();
+		Age pedro = new Age(21);
+		Age jorge = new Age(41);
+		Age maria = new Age(11);
+		list.addAll(Arrays.asList(pedro,jorge,maria));
+		Assertions.assertEquals(2, servicePerson.removeNotAdult(list).size());
 	}
 
 }
